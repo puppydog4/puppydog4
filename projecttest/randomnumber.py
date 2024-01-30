af17d8982adcfe4a2663c3b04c97276267b4e971
puppydog4 <158152797+puppydog4@users.noreply.github.com>
2024-01-30 22:05:11 +0000

    Create README.md

---
+++ projecttest/randomnumber.py
@@ -0,0 +1,31 @@
+import random
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"guess a number between 1 and {x}: "))
+        if guess < random_number:
+            print ("guess again . too low")
+        elif guess > random_number:
+            print("guess again too high")
+    print(f"you have guessed {x} correctly")
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ""
+    while feedback != "c":
+            if low != high:
+                guess = random.randint(low, high)
+            else:
+                guess = low
+            feedback = input(f"is {guess} too high (h) , too low(l), or correct (c) ??").lower()
+            if feedback == 'h':
+                high = guess - 1
+            elif feedback == 'l':
+                low = guess + 1
+
+    print(f"the computer guessed your number {guess} , correctly")
+
+
+computer_guess(10)
